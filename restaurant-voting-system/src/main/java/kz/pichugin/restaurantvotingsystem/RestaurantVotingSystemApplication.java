f32aa2d9dbb9c1280d9c91a13c524cd8ab91b205
Petr Pichugin <petr.pichugin@gmail.com>
2022-06-28 17:45:29 +0500

    initial commit - SpringBoot App initialization

---
+++ restaurant-voting-system/src/main/java/kz/pichugin/restaurantvotingsystem/RestaurantVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.pichugin.restaurantvotingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingSystemApplication.class, args);
+    }
+
+}
---
+++ restaurant-voting-system/src/test/java/kz/pichugin/restaurantvotingsystem/RestaurantVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.pichugin.restaurantvotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
