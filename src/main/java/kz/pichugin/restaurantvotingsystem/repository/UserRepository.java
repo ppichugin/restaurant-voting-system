c0dc2986f86bb9dfe6043bb6e8a37c809ce8b88a
Petr Pichugin <petr.pichugin@gmail.com>
2022-07-30 17:42:15 +0500

    impl data jpa for UserRepository

--- src/main/java/kz/pichugin/restaurantvotingsystem/repository/UserRepository.java
+++ src/main/java/kz/pichugin/restaurantvotingsystem/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package kz.pichugin.restaurantvotingsystem.repository;
 
 import kz.pichugin.restaurantvotingsystem.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface UserRepository extends BaseRepository<User> {
+
+    @Query("SELECT u FROM User u WHERE u.email =:email")
     Optional<User> getByEmail(String email);
 }
